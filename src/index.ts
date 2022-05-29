4cc8930965a22128db728a95e91fdce2a80340a6
arash oshnoudi <a.oshnoudi@demisco.ir>
2022-05-29 16:55:12 +0430

    new changes

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,19 @@ const builder = new QueryExpr();
 const q = builder
     .from("users")
     .select('name', 'fname')
-    // .where(c => {
-    //     c.and(c => {
-    //         c.eq("name", "Ali");
-    //         c.or((c) => {
-    //             c.gtn("age", 10);
-    //             c.eq("address", "isf");
-    //         });
-    //     })
-    // })
-    .orWhere(c => {
-        c.eq("name", "Ali");
-        c.and((c) => {
-            c.gt("age", 10);
-            c.lt("age", 20);
-            c.or(c => c.between("age", 40, 50))
-        });
+    .filter(c => {
+       c.or(c=>{
+        c.eq("name", "Arash");
+        c.in("name", "Ali", "Reza");
+        c.and(c=>{
+            c.between("age","?",20);
+        })
+       });
+       //c.eq("name","asghar");
+       
+        //c.not(c => c.in("name", "Ali", "Reza"));
     })
     .skip(0)
     .take(10)
-    .generate();
+     .generate();
 console.debug(q);
